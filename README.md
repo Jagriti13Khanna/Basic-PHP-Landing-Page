68b0735643f6f58f7acc0329dcfae797bdd5e929
Jagriti13Khanna <jagriti4k@gmail.com>
2022-03-20 22:35:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Introduction
+A simple PHP generated HTML landing page styled using CSS flexbox.
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+This is a landing page built using PHP. The site includes:
+* Heading(s), 
+* List(s), 
+* Image(s), 
+* Paragraph(s), 
+* Anchors, 
+* Basic styling
+all done using PHP and HTML.
+
+The styling is done based on CSS flexbox.
+
+## Technologies 
+* HTML5
+* CSS3
+* PHP
+
+## Setup
+* Download the zip file of the code.
+* Upload it to a server. You can also run it locally by downloading any open-source cross-platform web server solution stack package like xampp.
+* Open the index.php and voila, the PHP site is up and running.
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2025 landing page</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php echo"<header>
+        <h1>Welcome to DMIT 2025</h1>
+        <h2>PHP/MySQL</h2>
+        <hr class='wide'>
+    </header>";?>
+    <?php echo"<section class='description'>
+        <h3>COURSE DESCRIPTION</h3>
+        <hr>
+        <p>Building on the knowledge and skills gained in the prerequisites, students in this course will start creating web applications utilizing the leading open source server-side language and database. Successful students will be able to develop database-driven website features such as blogs, image galleries, content management, and online catalogs.
+            <ul>
+                <li>Course Credits: 4.50</li>
+                <li>Pre-requisites: DMIT1530</li>
+                <li>Equivalent Courses: DMIT222</li>
+            </ul>
+        </p>
+    </section>";?>
+    <?php 
+        echo" 
+        <section class='outcome'>
+            <h3>LEARNING OUTCOMES</h3>
+            <hr>
+            <table>
+                <tr>
+                    <th>OUTCOME</th>
+                    <th>Upon successful completion of this course, you will be able to</th>
+                </tr>
+                <tr>
+                    <td class='text-ct'>1</td>
+                    <td>Use the LAMP (Linux, Apache, MySQL, & PHP) environment and server-side technologies.</td>
+                </tr>
+                <tr>
+                    <td class='text-ct'>2</td>
+                    <td>Develop and use PHP and MySQL to process form-submitted data.</td>
+                </tr>
+                <tr>
+                    <td class='text-ct'>3</td>
+                    <td>Build a basic CRUD (Create, Read, Update, & Delete) web application with PHP and MySQL.</td>
+                </tr>
+                <tr>
+                    <td class='text-ct'>4</td>
+                    <td>Build a basic Blog application utilizing PHP and MySQL.</td>
+                </tr>
+                <tr>
+                    <td class='text-ct'>5</td>
+                    <td>Build a basic Image Gallery application utilizing PHP and MySQL.</td>
+                </tr>
+                <tr>
+                    <td class='text-ct'>6</td>
+                    <td>Design and develop an online catalogue using PHP and MySQL.</td>
+                </tr>
+            </table>
+        </section>";
+    ?>
+    <?php 
+        echo"
+            <section class='evaluation'>
+                <h3>STUDENT EVALUATION</h3>
+                <hr>
+                <p>A minimum passing grade of 50% is required.</p>
+                <img src='img/marks.JPG' alt='Mark Distribution'>
+            </section>";
+    ?>
+    <?php echo"<section class='flex'>
+        <div>
+            <h3>STUDENT RESPONSIBILITY</h3>
+            <hr>
+            <p>It is expected that students will be responsible citizens of the Institute by following the Student Rights and Responsibilities Policy(SR 1.0). As such, each student will assist in the preservation of Institute property, and assume responsibility for their education by staying informed of and abiding by academic requirements and policies; demonstrating respect toward others; and meet expectations concerning attendance, assignments, deadlines, and appointments.</p>
+            <p><a href='https://www.nait.ca/nait/about/corporate-structure/legal/nait-policies-procedures' target='_blank'>Read more about NAIT Policies & Procedures</a></p>                      
+        </div>
+        <div>
+            <h3>EQUITY AND INCLUSION STATEMENT</h3>
+            <hr>
+            <p>NAIT is committed to advancing equity and to actively and intentionally creating learning environments that promote a sense of belonging and dignity that ensure all people are safe, respected and valued. Acknowledging that every member of the NAIT community has a role in and responsibility to this work, NAIT provides the resources and support necessary for programs,
+            departments and individuals to champion equity, diversity and inclusion and address barriers in meaningful ways.</p>
+            <p><a href='https://www.nait.ca/nait/about/our-vision/equity-diversity-inclusion' target='_blank'>Read more about NAIT Equity & Inclusion</a></p>
+        </div>
+    </section>";?>
+    <?php echo"<footer>
+        <p>A LEADING POLYTECHNIC COMMITTED TO YOUR SUCCESS</p>
+        <p class='txt-right'>DMIT2025</p>
+    </footer>";?>
+
+    
+    
+
+    
+</body>
+</html>
